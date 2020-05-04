a53bfd0fd756bdc3b3c8d56878ed4fcd4342a36a
Jason Francki <jasonfrancki@gmail.com>
2020-05-04 07:45:27 -0500

    Added event listeners

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ const drinks = [
 ];
 
 const allIngredients = [];
+const allMissingIngredients = [ 'Cola' ];
 
 // MAIN
 
@@ -26,6 +27,7 @@ function makeDrink() {
 	const drinksList = document.querySelector('.drinks');
 
 	for (drink of drinks) {
+		// Create div for each drink
 		const div = document.createElement('div');
 		div.classList.add('drink');
 		div.innerHTML += `<h3>${drink.name}</h3>`;
@@ -34,41 +36,61 @@ function makeDrink() {
 		div.innerHTML += `<ul>`;
 		for (ing of drink.ingredients) {
 			div.innerHTML += `<li>${ing}</li>`;
-			console.log(ing);
 		}
-		// rumAndCoke.ingredients.forEach((e) => {
-		// 	drink.innerHTML += `<li>${e}</li>`;
-		// });
-		drink.innerHTML += `</ul>`;
-
+		div.innerHTML += `</ul>`;
 		drinksList.appendChild(div);
 
-		// add ingredients to master ingredients array
+		// Add ingredients to master ingredients array
 		drink.ingredients.forEach(function(ingredient) {
 			if (allIngredients.indexOf(ingredient) === -1) {
 				allIngredients.push(ingredient);
 			}
 		});
+
+		// if (drink.missingIngredients.length > 0) {
+		// 	div.style.backgroundColor = 'red';
+		// }
 	}
 
 	for (ing of allIngredients.sort()) {
 		ul.innerHTML += `<li>${ing}</li>`;
 	}
+}
 
-	// ul.appendChild(ul);
+function checkIngredients() {
+	const drinks = document.querySelectorAll('.drink');
+	for (drink of drinks) {
+		for (ing of allMissingIngredients) {
+			if (drink.innerHTML.indexOf(ing) !== -1) {
+				// drink.innerHTML[drink.innerHTML.indexOf(ing)].style.backgroundColor = 'black';
+				// drink.innerHTML.indexOf(ing).style.backgroundColor = 'green';
+				drink.style.backgroundColor = 'red';
+			} else {
+				console.log(drink.innerHTML.indexOf(ing));
+				drink.style.backgroundColor = 'green';
+			}
+		}
+	}
 
-	// // drink.innerHTML += `<h3>${}</h3>`;
-	// drink.innerHTML += `<ul>`;
-	// rumAndCoke.ingredients.forEach((e) => {
-	// 	drink.innerHTML += `<li>${e}</li>`;
-	// });
-	// drink.innerHTML += `</ul>`;
-
-	// drinksList.appendChild(drink);
+	const lis = document.querySelectorAll('li');
+	for (ing of allMissingIngredients) {
+		for (li of lis) {
+			if (ing.includes(li.textContent)) {
+				li.classList.add('lineThrough');
+				// li.style.textDecoration = 'line-through';
+			} else {
+				li.classList.remove('lineThrough');
+			}
+		}
+	}
+	// Select any li (main and footer?) that matches an allMissingIngredient and crossthrough
+	// if (allMissingIngredients.indexOf(ing) != -1) {
+	// 	div.classList.add('red');
+	// }
 }
 
 makeDrink();
-
+checkIngredients();
 // Loop through drink objects and create a div for each one with name, photo, description and
 // ingredient list
 
@@ -77,3 +99,17 @@ makeDrink();
 // List these in alphabetical order in the footer
 // OnClick of ingredient - toggle strikethrough on list in footer and each drink that contains
 // that ingredient, and change background to red and adjust opacity
+
+const lis = document.querySelectorAll('.allIngredients li');
+
+for (li of lis) {
+	li.addEventListener('click', () => {
+		// event.target.classList.toggle('lineThrough');
+		if (allMissingIngredients.indexOf(event.target.textContent) === -1) {
+			allMissingIngredients.push(event.target.textContent);
+		} else {
+			allMissingIngredients.pop(event.target.textContent);
+		}
+		checkIngredients();
+	});
+}
