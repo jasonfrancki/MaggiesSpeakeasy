dddcd9565f038f629390285bf193487621910a71
Jason Francki <jasonfrancki@gmail.com>
2020-08-02 16:53:53 -0500

    added more drinks

--- drinks.js
+++ drinks.js
@@ -1,6 +1,12 @@
 const drinks = [
 	{
 		description:
+			'Arrr you going to try this one?  A mix of peach, pineapple, coconut, honeydew melon and lemon lime.',
+		ingredients: [ 'Peach Schnapps', 'Coconut Rum', 'Melon Liqueur', 'Pineapple Juice', 'Lemon Lime Soda' ],
+		name: 'Angry Pirate'
+	},
+	{
+		description:
 			'This drink, which combines blue raspberry vodka, Island Punch Pucker, sweet and sour mix, pineapple juice, simple syrup, corn syrup, and blue sugar, is just as beautiful as any secluded beach we can think of. Add a lemon wheel on the rim for a slice of yellow sunshine.',
 		ingredients: [
 			'Island Punch Pucker',
@@ -24,10 +30,10 @@ const drinks = [
 		name: 'Purple People Eater'
 	},
 	{
-		description:
-			'This incredible vodka mixed drink recipe calls for vodka, coconut rum, Peach Schnapps, blue raspberry vodka, and lemon lime soda, and is garnished with Swedish Fish Candy and Nerds in order to make this bad boy look like a legitimate little fishbowl.',
-		ingredients: [ 'Vodka', 'Coconut Rum', 'Peach Schnapps', 'Blue Raspberry Vodka', 'Lemon Lime Soda' ],
-		name: 'Personal Fishbowl'
+		description: 'This mix of rum, coconut, pineapple, and orange is dangerously good.',
+		ingredients: [ 'White Rum', 'Coconut Rum', 'Blue Curacao', 'Pineapple Juice' ],
+		garnish: [ 'Pineapple', 'Cherry' ],
+		name: 'Sweet Poison'
 	},
 	{
 		description:
