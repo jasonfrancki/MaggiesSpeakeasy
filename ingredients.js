4bd153a759902fb3b50d0b57e9545858ca237437
Jason Francki <jasonfrancki@gmail.com>
2020-08-01 22:38:29 -0500

    Created ingredients array

---
+++ ingredients.js
@@ -0,0 +1,26 @@
+const ingredients = [
+	{
+		name: 'Cola',
+		inStock: true
+	},
+	{
+		name: 'Dark Rum',
+		inStock: true
+	},
+	{
+		name: 'Lime Juice',
+		inStock: true
+	},
+	{
+		name: 'Orange Curacao',
+		inStock: true
+	},
+	{
+		name: 'Orgeat Syrup',
+		inStock: true
+	},
+	{
+		name: 'White Rum',
+		inStock: true
+	}
+];
